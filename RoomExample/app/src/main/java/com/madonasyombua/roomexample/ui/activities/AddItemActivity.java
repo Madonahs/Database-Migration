1682a1dd55595784184660c1b51e75a57b400af5
madonahs <syombuamadona@gmail.com>
2019-11-14 13:25:02 -0500

    add room example files

---
+++ RoomExample/app/src/main/java/com/madonasyombua/roomexample/ui/activities/AddItemActivity.java
@@ -0,0 +1,14 @@
+package com.madonasyombua.roomexample.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
