c53c95dcc299eaf4c7a769f51b14dc889b8eacc9
madonahs <syombuamadona@gmail.com>
2019-11-23 12:55:26 -0500

    [-] This project might be unnecessary 🤔

--- SqliteExampleKotlin/app/src/main/java/com/madonasyombua/sqliteexamplekotlin/MainActivity.kt
+++ SqliteExampleKotlin/app/src/main/java/com/madonasyombua/sqliteexamplekotlin/MainActivity.kt
@@ -3,6 +3,11 @@ package com.madonasyombua.sqliteexamplekotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * @author madona
+ *
+ * This project might be unnecessary
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
